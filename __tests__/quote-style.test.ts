5818d0582cd6b7d97c70de1d91fe55593e071107
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2024-06-28 10:57:22 +0200

    Add test case for smart quotes in Markdown image links

--- __tests__/quote-style.test.ts
+++ __tests__/quote-style.test.ts
@@ -123,5 +123,17 @@ ruleTest({
         singleQuoteStyle: SingleQuoteStyles.SmartQuote,
       },
     },
+    {
+      testName: 'Make sure quotes in markdown image links get ignored',
+      before: dedent`
+        ![link](https://test.it 'Test title')
+      `,
+      after: dedent`
+        ![link](https://test.it 'Test title')
+      `,
+      options: {
+        singleQuoteStyle: SingleQuoteStyles.SmartQuote,
+      },
+    },
   ],
 });
